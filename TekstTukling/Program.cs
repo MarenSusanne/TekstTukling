7a54b55124724f4d1e9e76151651dc4ea292c94f
MarenSusanne <Marensut@getacademy.no>
2024-05-15 15:53:54 +0200

    Add project files.

---
+++ TekstTukling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("WELCOME! WELCOME! ONE AND ALL!");
+Console.WriteLine("TO MADAM MIMS WORDPLAY PALACE");
+Console.WriteLine("Choose your fate:");
+Console.WriteLine("1. Rotate Text");
+Console.WriteLine("2. Change Word");
+string answer = Console.ReadLine();
+if (answer == "1")
+{
+    Console.WriteLine("Enter Word:");
+    string word = Console.ReadLine();
+    string drow = "";
+    for (int i = word.Length-1; i >= 0; i--)
+    {
+        drow += word[i];
+        
+    }Console.Write(drow);
+}
+else if (answer == "2")
+{
+    Console.WriteLine("Enter Word:");
+    string word = Console.ReadLine();
+    string ward = "";
+    for (int i = 0; i < word.Length; i++)
+    {
+        if (word[i] == 'e')
+        {
+            ward += 'a';
+        }
+        else
+        {
+            ward += word[i];
+        }
+    }
+
+    Console.WriteLine(ward);
+}
